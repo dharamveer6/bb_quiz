88cd9398c959b286d05cffd37d62514d3e63e5d4
dharamveer6 <128466470+dharamveer6@users.noreply.github.com>
2024-03-13 18:33:52 +0530

    changes in dvs

--- controllers/questionbankcontroller.js
+++ controllers/questionbankcontroller.js
@@ -278,8 +278,254 @@ var insert_single_question=async(req,res,next)=>{
 }
 
 
-var add_single_question=async(req,res,next)=>{
+var add_bulk_question=async(req,res,next)=>{
+    var data=req.body;
+
+  
+    const schema = Joi.object({
+      
+        sub_id:Joi.string().max(100).required()
+  
+       
+      }).unknown("true");
+  
+      const { error } =  await schema.validateAsync(req.body);
+  
+      const {sub_id}=data
+  
+      if(!req.file){
+        throw new CreateError("FileUploadError","upload the excel file of questions")
+      }
+  
+  
+      
+      const buffer = req.file.buffer;
+          
+          
+         
+           
+        
+        
+      const workbook = new exceljs.Workbook();
+  
+      await workbook.xlsx.load(buffer);
+      // console.log(filepath)
+  
+      const worksheet = workbook.getWorksheet(1);
+    const column_change = [];
+  
     
+  
+    var raw_temp1=  worksheet.getRow(1)
+  
+    bigindex=0
+  
+    raw_temp1.eachCell((cell, colNumber) => {
+      // Check if the cell has a value (not empty)
+      bigindex+=1
+    });
+  
+  
+   
+  
+    for(let j=1;j<=bigindex;j++){
+  
+     var cell= raw_temp1.getCell(j);
+  
+     if (cell.hasRichText) {
+        // Initialize an empty string to store the plain text
+        let plainText = '';
+  
+        // Iterate through rich text runs and concatenate the text
+        cell.richText.forEach((richTextRun) => {
+          plainText += richTextRun.text;
+        });
+  
+        if (!plainText) {
+          throw new CreateError("CustomError",`empty cell in the 1st row`)
+        }
+  
+        // Push the plain text to the 'data' array
+        column_change.push(plainText.trim());
+      }
+  
+      else {
+        // If the cell does not have rich text, check for null or undefined values
+        if (cell.value === null || cell.value === undefined) {
+          throw new CreateError("CustomError",`empty cell in the 1st row`)
+        } else {
+          column_change.push(cell.value.trim());
+        }
+      }
+  
+  
+    }
+  
+  
+  
+  
+  
+  
+   
+  // check columns name correct or not 
+  
+  var check_val_ar=[
+    "question", "option1",
+    "option2", "option3", "option4", "ans",
+    
+  ]
+  
+  console.log(column_change)
+  
+  
+  function check_column_fun(columns,check_val_ar){
+     for(let i of columns){
+  
+      
+        if (check_val_ar.indexOf(i) == -1) {
+          
+            return false
+          
+          }
+     }
+     return true
+  }
+  
+  var_for_excelformate=check_column_fun(column_change,check_val_ar);
+  
+  if(!var_for_excelformate){
+    throw new CreateError("FileUploadError","incorrect excel format")
+  }
+  
+  let lastRow = worksheet.rowCount; // Start with the last row
+    while (lastRow > 1) {
+      const row = worksheet.getRow(lastRow);
+      const rowData = row.values.map((value) => value ? value.toString() : '');
+      const isRowEmpty = rowData.every((value) => value.trim() === '');
+  
+      if (!isRowEmpty) {
+        break;
+      }
+  
+      lastRow--;
+    }
+  
+  
+    var bulk_insert=[]
+  
+  
+  
+    for(let i =2;i<=lastRow;i++){
+  
+      let data=[]
+  
+      var raw_temp=worksheet.getRow(i);
+   
+    for(let j=1;j<=bigindex;j++){
+  
+     var cell= raw_temp.getCell(j);
+  
+     if (cell.value?.richText) {
+      // console.log("check in rich")
+      // Initialize an empty string to store the plain text
+      let plainText ='';
+  
+      // Iterate through rich text runs and concatenate the text
+      cell.value.richText.forEach((richTextRun) => {
+        plainText += richTextRun.text;
+      });
+  
+      if (!plainText) {
+        throw new CreateError("CustomError",`empty cell in the ${i} row  and column is ${j}`)
+      }
+  
+      console.log(plainText)
+  
+    
+  
+      // Push the plain text to the 'data' array
+      data.push(plainText);
+    }
+  
+    else {
+      // console.log(cell.value)
+      // If the cell does not have rich text, check for null or undefined values
+      if (cell.value === null || cell.value === undefined) {
+        throw new CreateError("CustomError",`empty cell in the ${i} row and column is ${j}`)
+      } else {
+  
+        var val=cell.value.toString()
+     
+          data.push(val);
+        
+      }
+    }
+  
+  
+   
+  
+    }
+   
+  
+       
+  
+  
+       
+  
+  
+   
+  
+        let insert_data={}
+  
+        for (let j in check_val_ar){
+          
+          insert_data[check_val_ar[j]]=data[j]
+  
+        }
+  
+        var final_data={...insert_data,sub_id:sub_id}
+  
+        bulk_insert.push(final_data);
+  
+        if(bulk_insert.length >= 1000){
+  
+          const sen1=JSON.stringify(bulk_insert);
+          const channel=await consumeMessages();
+   
+      
+          channel.sendToQueue("store_prerec__question_excel", Buffer.from(sen1));
+  
+         
+      
+              bulk_insert=[]
+      
+            }
+  
+  
+  
+  
+      }
+  
+      // console.log(bulk_insert)
+  
+      if(bulk_insert.length!==0){
+       
+  
+        const channel=await consumeMessages();
+  
+        const sen1=JSON.stringify(bulk_insert);
+    
+     
+        
+        channel.sendToQueue("store_prerec__question_excel", Buffer.from(sen1));
+        bulk_insert=[]
+      }
+      
+  
+     
+  
+      res.send({status:1,msg:"excel upload succesfully"})
+  
 }
 
 
