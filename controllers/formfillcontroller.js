7c76a5c4092e4dae981cbe65d2bb9b38ca343e34
Ulquiorra1237 <130816462+Ulquiorra1237@users.noreply.github.com>
2024-03-19 12:30:25 +0530

    changes

--- controllers/formfillcontroller.js
+++ controllers/formfillcontroller.js
@@ -7,38 +7,72 @@ const Subject = require("../models/subjectmodel");
 
 
 var get_all_categories = async(req,res,next)=>{
-    const categories = await Category.find({}, 'category_name');
-        res.json({
-            status: 1,
-            categories: categories
-        });
+    const schema = Joi.object({
+        search: Joi.string().max(50).allow('').required() // Allow empty string as default value
+    });
+
+    const { error } = await schema.validateAsync(req.query);
+
+    // Extract search variable from query parameters
+    const { search } = req.query;
+
+    // Construct search filter
+    const searchFilter = search ? { category_name: { $regex: new RegExp(search, 'i') } } : {};
+
+    // Find categories based on search filter
+    const categories = await Category.find(searchFilter, 'category_name');
+
+    // Return the result
+    res.json({ status: 1, categories: categories });
 
 }
 
 
 var get_all_sub_categories = async(req,res,next)=>{
-    // Fetch all sub-categories from the database
-    const subCategories = await SubCategory.distinct('sub_category_name');
-    res.json({
-        status: 1,
-        sub_categories: subCategories
+    // Validate request parameters
+    const schema = Joi.object({
+        search: Joi.string().max(50).allow('').required() // Allow empty string as default value
     });
+
+    const { error } = await schema.validateAsync(req.query);
+
+    // Extract search variable from query parameters
+    const { search } = req.query;
+
+    // Construct search filter
+    const searchFilter = search ? { sub_category_name: { $regex: new RegExp(search, 'i') } } : {};
+
+    // Find subcategories based on search filter
+    const subCategories = await SubCategory.find(searchFilter, 'sub_category_name');
+
+    // Extract distinct subcategory names
+    const distinctSubCategories = [...new Set(subCategories.map(subCategory => subCategory.sub_category_name))];
+
+    // Return the result
+    res.json({ status: 1, sub_categories: distinctSubCategories });
 }
 
 
 var get_all_subjects_from_subcategories = async(req,res,next)=>{
     
 
-    const schema = Joi.object({
-        sub_ids: Joi.array().items(Joi.string().required())
+     // Validate request body
+     const schema = Joi.object({
+        sub_ids: Joi.array().items(Joi.string().required()).required(),
+        search: Joi.string().max(50).allow('').required() // Allow empty string as default value
     });
 
     const { error } = await schema.validateAsync(req.body);
+   
 
-    const { sub_ids } = req.body;
+    // Extract sub_ids and search from request body
+    const { sub_ids, search } = req.body;
 
-    // Fetch all subjects matching the IDs
-    const subjects = await Subject.find({ sub_cat_id: { $in: sub_ids } });
+    // Construct search filter
+    const searchFilter = search ? { sub_name: { $regex: new RegExp(search, 'i') } } : {};
+
+    // Fetch subjects matching the IDs and search filter
+    const subjects = await Subject.find({ sub_cat_id: { $in: sub_ids }, ...searchFilter });
 
     // Extract unique sub_names from subjects
     const uniqueSubNames = new Set();
@@ -51,6 +85,7 @@ var get_all_subjects_from_subcategories = async(req,res,next)=>{
     // Convert the set to an array of unique sub_names
     const subNames = Array.from(uniqueSubNames);
 
+    // Return the result
     res.json({ status: 1, data: subNames });
 
 
