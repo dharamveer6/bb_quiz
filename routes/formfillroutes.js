a569a20249eb7eb5d2a659c8dcf184e3b1b842e0
Ulquiorra1237 <130816462+Ulquiorra1237@users.noreply.github.com>
2024-03-20 10:20:15 +0530

    changes

--- routes/formfillroutes.js
+++ routes/formfillroutes.js
@@ -7,7 +7,7 @@ const formfillroutes = express.Router()
 
 formfillroutes.route('/get/all/category').post(userAuthMiddleware,get_all_categories);
 formfillroutes.route('/get/all/subcategory').post(userAuthMiddleware,get_all_sub_categories);
-formfillroutes.route('/get/all/subjects/from/subcategories').post(get_all_subjects_from_subcategories);
+formfillroutes.route('/get/all/subjects/from/subcategories').post(userAuthMiddleware,get_all_subjects_from_subcategories);
 formfillroutes.route('/add/quiz').post(add_Quiz);
 
 
