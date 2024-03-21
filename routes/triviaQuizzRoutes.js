d21b5d0f8426add645074a682b3a46f1d188523f
aishwarya8989 <aishwaryabeniwal89@gmail.com>
2024-03-21 18:32:23 +0530

    dsd

--- routes/triviaQuizzRoutes.js
+++ routes/triviaQuizzRoutes.js
@@ -1,10 +1,11 @@
 const express=require('express');
-const { createTriviaQuizz, getQuizz, viewDetails } = require('../controllers/triviaQuizzController');
+const { createTriviaQuizz, getQuizz, viewDetails, chnageBanner } = require('../controllers/triviaQuizzController');
 const { uploadimage } = require('../utils/imgmulter');
 const triviaRoute=express.Router();
 
 triviaRoute.route('/create/quizz').post(uploadimage.single('banner'),createTriviaQuizz);
 triviaRoute.route('/get/quizz').get(getQuizz);
 triviaRoute.route('/view/details').get(viewDetails);
+triviaRoute.route('/change/banner').get(uploadimage.single('banner'),chnageBanner);
 
 module.exports={triviaRoute}
