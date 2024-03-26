0485fd3cd5ae6d203ac7b49c71b2e48981bf2387
Anandshivangi <shivangishweta16@gmail.com>
2024-03-26 17:53:32 +0530

    f

--- controllers/studenttriviaquizcontroller.js
+++ controllers/studenttriviaquizcontroller.js
@@ -13,10 +13,10 @@ const Result_Subtrivia = require('../models/result_subtrivia.js');
 
 
 
-var join_quiz_by_student=async(req,res,next)=>{
-    const schema = Joi.object({
-      subtrivia_id: Joi.string().max(250).required(),
-        participant_id: Joi.string().required(),
+var join_quiz_by_student = async (req, res, next) => {
+  const schema = Joi.object({
+    subtrivia_id: Joi.string().max(250).required(),
+    participant_id: Joi.string().required(),
 
 
   });
@@ -25,137 +25,58 @@ var join_quiz_by_student=async(req,res,next)=>{
 
   const { subtrivia_id, participant_id } = req.body;
 
-    const data=await SubTriviaQuiz.findOne({
-        _id: new mongoose.Types.ObjectId(subtrivia_id),
-      });
+  const data = await SubTriviaQuiz.findOne({
+    _id: new mongoose.Types.ObjectId(subtrivia_id),
+  });
 
-      let count=0
-      var question=[];
-      var check=0
-      var remain_question=0
+  let count = 0
+  var question = [];
+  var check = 0
+  var remain_question = 0
 
   var ans = [];
 
-      var stu_ans=[]
+  var stu_ans = []
 
-      console.log(data.question_composition)
+  console.log(data.question_composition)
 
-      const question_composition2 = Object.fromEntries(data.question_composition.entries());
+  const question_composition2 = Object.fromEntries(data.question_composition.entries());
 
-      console.log(question_composition2)
-
-
-
-      for (let i in question_composition2) {
-
-        console.log(i)
-        check++;
-       
-       
-        const persentage = question_composition2[i];
-        
-        
-
-
-       
-        console.log(data.total_num_of_quest,"total no of questions");
-        var single_tag_quest =Math.floor(persentage / 100 * data.total_num_of_quest);
-        
-
-        remain_question+=single_tag_quest
-        
-
-        if(check == count){
-          const data=req.body.total_num_of_quest-remain_question;
-         
-          single_tag_quest+=data
-
-        }
-
-        console.log(single_tag_quest,"sin")
-
-        
-
-  
-
-         
-        
+  console.log(question_composition2)
 
 
 
-        var que = await Question.aggregate([
-            // Stage 1: Match documents where is_del is 0
-            { 
-                $match: { 
-                    is_del: 0 
-                } 
-            },
-            // Stage 2: Sample `single_tag_quest` documents from the matched documents
-            { 
-                $sample: { 
-                    size: single_tag_quest 
-                } 
-            },
-            // Stage 3: Project specific fields
-            {
-                $project: {
-                  // Include or exclude as needed
-                    sub_id: 0, // Include or exclude as needed
-                  
-                   // Include or exclude as needed
-                }
-            }
-        ]);
+  for (let i in question_composition2) {
 
-        console.log(que,"questions")
-
-        const val2=single_tag_quest-que.length;
-
-        if(val2>0){
-            const que2 = await Question.aggregate([
-                // Stage 1: Match documents where is_del is 0
-                { 
-                    $match: { 
-                        is_del: 1
-                    } 
-                },
-                // Stage 2: Sample `single_tag_quest` documents from the matched documents
-                { 
-                    $sample: { 
-                        size: val2 
-                    } 
-                },
-                // Stage 3: Project specific fields
-                {
-                    $project: {
-                      // Include or exclude as needed
-                        sub_id: 0, // Include or exclude as needed
-                       // Include or exclude as needed
-                        // Include or exclude as needed
-                    }
-                }
-            ]);
-
-            que=[...que,...que2]
-        }
+    console.log(i)
+    check++;
 
 
-        for (let i of que) {
-          question = [...question, i._id];
-          ans = [...ans, i.ans];
-          stu_ans.push(-1)
-        }
-     
+    const persentage = question_composition2[i];
+
+
+
+
+
+    console.log(data.total_num_of_quest, "total no of questions");
+    var single_tag_quest = Math.floor(persentage / 100 * data.total_num_of_quest);
+
+
+    remain_question += single_tag_quest
+
+
+    if (check == count) {
+      const data = req.body.total_num_of_quest - remain_question;
+
+      single_tag_quest += data
 
     }
 
+    console.log(single_tag_quest, "sin")
 
 
-console.log(data.total_num_of_quest)
-console.log(data.time_per_question)
-console.log("hey")
-const secondsToAdd = (data.total_num_of_quest*data.time_per_question)+30; // Change this to the number of seconds you want to add
-const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
+
+
 
 
 
@@ -180,11 +101,13 @@ const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
         $project: {
           // Include or exclude as needed
           sub_id: 0, // Include or exclude as needed
-          is_del: 0,// Include or exclude as needed
-          ans: 0,// Include or exclude as needed
+
+          // Include or exclude as needed
         }
       }
     ]);
+
+    console.log(que, "questions")
 
     const val2 = single_tag_quest - que.length;
 
@@ -207,8 +130,8 @@ const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
           $project: {
             // Include or exclude as needed
             sub_id: 0, // Include or exclude as needed
-            is_del: 0,// Include or exclude as needed
-            ans: 0,// Include or exclude as needed
+            // Include or exclude as needed
+            // Include or exclude as needed
           }
         }
       ]);
@@ -220,38 +143,115 @@ const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
     for (let i of que) {
       question = [...question, i._id];
       ans = [...ans, i.ans];
+      stu_ans.push(-1)
     }
 
 
-    if (question.length !== ans.length) {
-      throw new CreateError("CustomError", "question array and answer array is not same")
-    }
   }
 
 
-  const start_time = moment().valueOf()
 
-
+  console.log(data.total_num_of_quest)
+  console.log(data.time_per_question)
+  console.log("hey")
   const secondsToAdd = (data.total_num_of_quest * data.time_per_question) + 30; // Change this to the number of seconds you want to add
   const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
 
 
-  const result = await Result_Subtrivia.findOneAndUpdate(
-    { participant_id: participant_id, subtrivia_id: subtrivia_id },
-    { $set: {stu_ans,is_attempted:1, end_time: end_time, start_time: start_time, cor_ans: ans, questions: question,participant_id: participant_id, subtrivia_id: subtrivia_id ,stu_ans,submit_time_period:secondsToAdd} },
-    { upsert: true, new: true }
-  );
-
-  // If you need to perform additional processing after updating or inserting the document
-  // (e.g., checking whether the condition was met), you can do so here
-
-  // Save the document
-  await result.save();
 
 
 
 
-res.send({status:1,que,timeperiod:secondsToAdd,stu_ans})
+  var que = await Question.aggregate([
+    // Stage 1: Match documents where is_del is 0
+    {
+      $match: {
+        is_del: 0
+      }
+    },
+    // Stage 2: Sample `single_tag_quest` documents from the matched documents
+    {
+      $sample: {
+        size: single_tag_quest
+      }
+    },
+    // Stage 3: Project specific fields
+    {
+      $project: {
+        // Include or exclude as needed
+        sub_id: 0, // Include or exclude as needed
+        is_del: 0,// Include or exclude as needed
+        ans: 0,// Include or exclude as needed
+      }
+    }
+  ]);
+
+  const val2 = single_tag_quest - que.length;
+
+  if (val2 > 0) {
+    const que2 = await Question.aggregate([
+      // Stage 1: Match documents where is_del is 0
+      {
+        $match: {
+          is_del: 1
+        }
+      },
+      // Stage 2: Sample `single_tag_quest` documents from the matched documents
+      {
+        $sample: {
+          size: val2
+        }
+      },
+      // Stage 3: Project specific fields
+      {
+        $project: {
+          // Include or exclude as needed
+          sub_id: 0, // Include or exclude as needed
+          is_del: 0,// Include or exclude as needed
+          ans: 0,// Include or exclude as needed
+        }
+      }
+    ]);
+
+    que = [...que, ...que2]
+  }
+
+
+  for (let i of que) {
+    question = [...question, i._id];
+    ans = [...ans, i.ans];
+  }
+
+
+  if (question.length !== ans.length) {
+    throw new CreateError("CustomError", "question array and answer array is not same")
+  }
+}
+
+
+const start_time = moment().valueOf()
+
+
+const secondsToAdd = (data.total_num_of_quest * data.time_per_question) + 30; // Change this to the number of seconds you want to add
+const end_time = moment(start_time).add(secondsToAdd, 'seconds').valueOf();
+
+
+const result = await Result_Subtrivia.findOneAndUpdate(
+  { participant_id: participant_id, subtrivia_id: subtrivia_id },
+  { $set: { stu_ans, is_attempted: 1, end_time: end_time, start_time: start_time, cor_ans: ans, questions: question, participant_id: participant_id, subtrivia_id: subtrivia_id, stu_ans, submit_time_period: secondsToAdd } },
+  { upsert: true, new: true }
+);
+
+// If you need to perform additional processing after updating or inserting the document
+// (e.g., checking whether the condition was met), you can do so here
+
+// Save the document
+await result.save();
+
+
+
+
+res.send({ status: 1, que, timeperiod: secondsToAdd, stu_ans })
 
 
 
--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const { azurestreamroute } = require('./routes/azurestreamroutes');
 const { formfillroutes } = require('./routes/formfillroutes');
 const { triviaRoute } = require('./routes/triviaQuizzRoutes');
 const { quizRoute } = require('./routes/quizRoutes');
-const { studymaterialRoute } = require('./routes/studymaterialroutes');
-const { dailyupdateRoute } = require('./routes/dailyupdateroutes');
 const { triviastudentrouter } = require('./routes/studenttriviarouter');
 
 const app = express();
@@ -41,8 +39,8 @@ app.use('/questionbank', questionbankRoute);
 app.use('/formfill', formfillroutes)
 app.use('/stream', azurestreamroute)
 app.use('/trivia', triviaRoute)
-app.use('/student/trivia',triviastudentrouter)
-app.use('/active/quiz', quizRoute)  
+app.use('/student/trivia', triviastudentrouter)
+app.use('/active/quiz', quizRoute)
 
 
 
