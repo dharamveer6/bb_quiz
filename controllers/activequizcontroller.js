9fdd4dc94e77c96fbd4721d00b2b4902f17370d5
dharamveer6 <128466470+dharamveer6@users.noreply.github.com>
2024-03-28 16:07:35 +0530

    fasasf

--- controllers/activequizcontroller.js
+++ controllers/activequizcontroller.js
@@ -88,7 +88,7 @@ let createActiveQuiz = async (req, res, next) => {
     subjects_id,
     question_composition,
     total_num_of_quest,
-    time_per_ques,
+    time_per_question,
     min_reward_per,
     reward,
     rules,
@@ -247,7 +247,7 @@ let createActiveQuiz = async (req, res, next) => {
     subjects_id,
     question_composition,
     total_num_of_quest,
-    time_per_ques,
+    time_per_question,
     min_reward_per,
     reward,
     rules,
@@ -261,7 +261,7 @@ let createActiveQuiz = async (req, res, next) => {
   const Active_Quiz_Id = add.id || add._id;
 
 
-  var seconds_to_add = time_per_ques * time_per_ques
+  var seconds_to_add = time_per_question * total_num_of_quest
 
   var end_time = moment(sch_time).add(seconds_to_add, 'seconds').valueOf();
 
@@ -271,7 +271,7 @@ let createActiveQuiz = async (req, res, next) => {
     subjects_id,
     question_composition,
     total_num_of_quest,
-    time_per_ques,
+    time_per_question,
     min_reward_per,
     reward,
     rules,
